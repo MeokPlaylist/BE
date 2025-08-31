76b3d305ee011348eb8307d3c9d1edd373a7fe9f
tmdgus0245 <tmdgus0245@ajou.ac.kr>
2025-08-31 23:17:52 +0900

    8-31

--- src/main/java/com/meokplaylist/api/controller/SocialInteractionController.java
+++ src/main/java/com/meokplaylist/api/controller/SocialInteractionController.java
@@ -2,10 +2,7 @@ package com.meokplaylist.api.controller;
 
 import com.meokplaylist.domain.service.SocialInteractionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
