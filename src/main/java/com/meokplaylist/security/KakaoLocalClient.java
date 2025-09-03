b71fbbd326a92c843c9de53a929d02eb6c6d8855
tmdgus0245 <tmdgus0245@ajou.ac.kr>
2025-09-03 23:35:46 +0900

    tourApi testing

--- src/main/java/com/meokplaylist/security/KakaoLocalClient.java
+++ src/main/java/com/meokplaylist/security/KakaoLocalClient.java
@@ -1,27 +1,20 @@
 package com.meokplaylist.security;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import com.meokplaylist.api.dto.KakaoSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Mono;
 
 
 import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class KakaoLocalClient {
 
-    private final WebClient wc;
-    public KakaoLocalClient(@Qualifier("kakaoWebClient") WebClient wc) {
-        this.wc = wc;
-    }
+    private final WebClient webClient = WebClient.builder().build();
 
     private static int RADIUS = 1000; //3m //테스트 일떄는 1km로 설정
 
@@ -31,26 +24,24 @@ public class KakaoLocalClient {
     @Value("${kakao.local.rest-api-key}")
     private String restApiKey;
 
-
     public KakaoSearchResponse searchByCategory(String category, double x, double y,
                                                 Integer page, Integer size) {
-        return wc.get()
-                .uri(b -> b.path("/v2/local/search/category.json")
-                        .queryParam("category_group_code", category)
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https")
+                        .host(baseUrl.replace("https://", "").replace("http://",""))
+                        .path("/v2/local/search/category.json")
+                        .queryParam("category_group_code", category) // ex) FD6:음식점, CE7:카페
                         .queryParam("x", x)
                         .queryParam("y", y)
-                        .queryParam("radius", RADIUS)
-                        .queryParamIfPresent("page", Optional.ofNullable(page))
-                        .queryParamIfPresent("size", Optional.ofNullable(size))
-                        .queryParam("sort","distance")
+                        .queryParam("radius", RADIUS)                // 0~20000 (m)
+                        .queryParamIfPresent("page", Optional.ofNullable(page)) // 1~45
+                        .queryParamIfPresent("size", Optional.ofNullable(size)) // 1~15
+                        .queryParam("sort","distance") //거리 순으로 정렬
                         .build())
+                .header(HttpHeaders.AUTHORIZATION, "KakaoAK " + restApiKey)
                 .retrieve()
-                .onStatus(HttpStatusCode::isError, resp ->
-                        resp.bodyToMono(String.class)
-                                .flatMap(body -> Mono.error(new ResponseStatusException(
-                                        resp.statusCode(), "Kakao API error: " + body))))
                 .bodyToMono(KakaoSearchResponse.class)
                 .block();
     }
-
 }
